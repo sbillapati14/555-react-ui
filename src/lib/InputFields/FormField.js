caa6f39766ce9a55ba5bea11587929cf35e3d716
Dan Poblete <ace.poblete@gmail.com>
2018-02-08 16:19:25 -0600

    adding formfield

---
+++ src/lib/InputFields/FormField.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FormField extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default FormField;
