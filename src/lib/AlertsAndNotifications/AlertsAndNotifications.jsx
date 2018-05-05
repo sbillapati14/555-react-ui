ac288e2e534b1d938ba603e45c1410ca534ae4ee
santhoshibillapati <santhu14>
2018-05-05 12:16:21 -0400

    changing some css styles.

--- src/lib/AlertsAndNotifications/AlertsAndNotifications.jsx
+++ src/lib/AlertsAndNotifications/AlertsAndNotifications.jsx
@@ -1,48 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import List, { ListItem } from 'material-ui/List';
+import { withStyles } from 'material-ui/styles';
+import Divider from 'material-ui/Divider';
+import Dot from 'material-ui-icons/Brightness1'
 
 
-const styles = {
-    notificationList:{
-        padding: '6px 20px 24px 37px',
-    },
-    listItem:{
-        borderBottom: '1px solid #eaeaea',
-        padding: '11px 0',
-        position: 'relative',
-    },
-    alert:{
-        color: '#5e5e5e',
-        fontSize: '14px',
-        fontWeight: '400',
-        fontFamily: '"Montserrat", "Helvetica", "Arial", sans-serif',
-        lineHeight: '19px',
-        paddingRight: '50px',
-        display: 'inline'
-    },
-    alertTime:{
-        color: '#a5a5a5',
-        fontSize: '12px',
-        fontWeight: '400',
-        lineHeight: '14px',
-        fontFamily: '"Montserrat", "Helvetica", "Arial", sans-serif',
-        position: 'absolute',
-        right: '0',
-        top: '16px',
-        display: 'inline'
+const styles = theme => {
+    return {
+        notificationList:{
+            padding: '0px 20px 10px 27px',
+            margin: '0',
+        },
+        listItem:{
+            padding: '0',
+        },
+        alert:{
+            color: '#5e5e5e',
+            fontSize: '14px',
+            fontWeight: '400',
+            fontFamily: '"Montserrat", "Helvetica", "Arial", sans-serif',
+            lineHeight: '19px',
+            paddingRight: '50px',
+            display: 'inline'
+        },
+        circle: {
+            width: 5,
+            height: 5,
+            position: 'relative',
+            left: '-14px',
+        },
+        alertTime:{
+            color: '#a5a5a5',
+            fontSize: '12px',
+            fontWeight: '400',
+            lineHeight: '14px',
+            fontFamily: '"Montserrat", "Helvetica", "Arial", sans-serif',
+            position: 'absolute',
+            right: '0',
+            top: '16px',
+            display: 'inline'
+        }
     }
-};
+    };
 
-const AlertsAndNotifications  = ({notifications, title, showTitle}) => {
+const AlertsAndNotifications  = ({notifications, title, showTitle, classes}) => {
     return(
-        <ul style={styles.notificationList}>
-            {notifications.map((notification, ind)=>
-                (<li key={ind} style={styles.listItem}>
-                    <p style={styles.alert}>{notification.text}</p>
-                    <span style={styles.alertTime}>{notification.time}</span>
-                </li>)
-            )}
-        </ul>
+                <List className={classes.notificationList}>
+                {notifications.map((notification, ind)=>
+                  (<div>
+                      <ListItem key={ind} className={classes.listItem}>
+                          <Dot style={{ color:  "#000"}} className={classes.circle} />
+                          <p className={classes.alert}>{notification.text}</p>
+                    <span className={classes.alertTime}>{notification.time}</span>
+                  </ListItem>
+                  {(ind < notifications.length-1) && <Divider />}
+                </div>)
+                )}
+                </List>
     )
 }
 
@@ -59,4 +74,4 @@ AlertsAndNotifications.defaultProps = {
     ]
 }
 
-export default AlertsAndNotifications;
+export default withStyles(styles)(AlertsAndNotifications);
