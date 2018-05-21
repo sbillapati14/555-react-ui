d11798f5a5028561a58929c35cc48ae5a640ed06
santhoshibillapati <santhu14>
2018-05-21 10:33:09 -0400

    Merge branch 'master' of https://github.com/iris-platform/iris-react-ui into Package.json
    # Conflicts:
    #	src/demo/App.js

--- src/demo/App.js
+++ src/demo/App.js
@@ -67,7 +67,7 @@ class App extends Component {
   handleClose = () => {
     this.setState({ notification: false});
   };
-  copyToClipboard(){debugger;
+  copyToClipboard(){
     const el = document.createElement('textarea');
     el.value = this.state.cpyToClip;
     document.body.appendChild(el);
