9e29dac693fa3c0932749f6177a8c5771d73eece
sbilla103 <sbillapati14>
2018-08-07 10:22:40 -0400

    Added Healthchart D3 component and updated D3 version.

--- src/demo/App.js
+++ src/demo/App.js
@@ -24,12 +24,13 @@ import Pagination from '../lib/Pagination';
 import AppDrawer from '../lib/AppDrawer';
 import AppContent from '../lib/AppContent';
 import AlertsAndNotifications from '../lib/AlertsAndNotifications';
+import HealthChart from '../lib/HealthChart';
 
 import OutlineButton from '../lib/Button/OutlineButton';
 import GradientButton from '../lib/Button/GradientButton';
 import AccentButton from '../lib/Button/AccentButton';
 import PaperCard from '../lib/PaperCard';
-import { BarChart } from '../lib/Graphs';
+// import { BarChart } from '../lib/Graphs';
 import { TextField, FormField } from '../lib/InputFields';
 import {Switch, SwitchButton} from '../lib/Switch';
 import FilterStatus from '../lib/DropDown/FilterStatus';
@@ -301,7 +302,7 @@ class App extends Component {
 
   <br/>
 
-      <PaperCard
+      {/* <PaperCard
         title="Charts"
         avatar={
           <Avatar>
@@ -315,7 +316,18 @@ class App extends Component {
           chartHeight={300}
           barWidth={35}/>
 
+      </PaperCard> */}
+
+      <PaperCard
+        title="Charts"
+        avatar={
+          <Avatar>
+            <ImageIcon />
+          </Avatar>
+        }>
+          <HealthChart/>
       </PaperCard>
+      
                 </PageLeft>
 
                 <PageRight>
--- src/demo/index.js
+++ src/demo/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import HealthChart from '../lib/HealthChart'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
